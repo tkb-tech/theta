703673a98b44ba80fff5180a1abd3111dbeac6ee
tkb-tech <61821413+tkb-tech@users.noreply.github.com>
2021-11-08 13:13:34 +0900

    v.1.1.1
    コードの形を少し整えました。

--- test_11_8.py
+++ test_11_8.py
@@ -62,66 +62,7 @@ comp = [
 "eGFR"
 ] #component
 
-"""
-総蛋白
-アルブミン
-CK
-AST
-ALT
-LD
-ALP
-γ-GTP
-CRP
-Cre
-尿酸
-BUN
-血清血糖
-中性脂肪
-T-chol
-HDL-chol
-LDL-chol
-Na
-K
-Cl
-Ca
-P
-T-bil
-D-bil
-BNP
-HbA1c
-グリコアルブミン
-WBC
-RBC
-Hb
-Ht
-Plt
-MCV
-MCHC
-好塩基球
-好酸球
-好中球
-桿状
-分葉
-新FDP定量
-D-dimer
-PT-INR
-APTT-time
-フィブリノゲン
-アンチトロンビンⅢ
-HBsAg（定量）
-HCVAb(Index)
-RPR定性（LA法）
-梅毒TP定性
-ケトン体
-潜血
-ウロビリノゲン
-白血球
-亜硝酸塩
-赤血球
-白血球
-偏平上皮細胞
-eGFR
-"""
+
 
 unit = [
 "g/dL",
@@ -183,6 +124,104 @@ unit = [
 "/HPF",
 "ml/min"
 ]
+print("成分の種類は"+str(len(comp)))
+
+value = []
+
+for i in range(len(comp)):
+    print(comp[i])
+    x = raw_input()
+    value.append(x)
+
+flag = 0
+cnt = len(value) - 1
+while flag == 0:
+    if value[cnt] != "null":
+        flag = 810
+    else:
+        cnt = cnt - 1
+
+print("値がnullでない最後の成分は"+comp[cnt])
+
+print("-------------------------------------------------------")
+
+for i in range(cnt):
+    if value[i] != "null":
+        if unit[i] != "null":
+            sys.stdout.write(comp[i]+" "+value[i]+" "+unit[i]+", ")
+        else:
+            sys.stdout.write(comp[i]+" "+value[i]+", ")
+
+if value[cnt] != "null":
+    if unit[cnt] != "null":
+        print(comp[cnt]+" "+value[cnt]+" "+unit[cnt])
+    else:
+        print(comp[cnt]+" "+value[cnt])
+
+print("-------------------------------------------------------")
+
+"""
+総蛋白
+アルブミン
+CK
+AST
+ALT
+LD
+ALP
+γ-GTP
+CRP
+Cre
+尿酸
+BUN
+血清血糖
+中性脂肪
+T-chol
+HDL-chol
+LDL-chol
+Na
+K
+Cl
+Ca
+P
+T-bil
+D-bil
+BNP
+HbA1c
+グリコアルブミン
+WBC
+RBC
+Hb
+Ht
+Plt
+MCV
+MCHC
+好塩基球
+好酸球
+好中球
+桿状
+分葉
+新FDP定量
+D-dimer
+PT-INR
+APTT-time
+フィブリノゲン
+アンチトロンビンⅢ
+HBsAg（定量）
+HCVAb(Index)
+RPR定性（LA法）
+梅毒TP定性
+ケトン体
+潜血
+ウロビリノゲン
+白血球
+亜硝酸塩
+赤血球
+白血球
+偏平上皮細胞
+eGFR
+"""
+
+
 """
 g/dL
 g/dL
@@ -243,40 +282,3 @@ T.U.
 /HPF
 ml/min
 """
-print(len(comp))
-print(len(unit))
-
-value = []
-
-for i in range(len(comp)):
-    print(comp[i])
-    x = raw_input()
-    value.append(x)
-
-flag = 0
-cnt = len(value) - 1
-while flag == 0:
-    if value[cnt] != "null":
-        flag = 810
-    else:
-        cnt = cnt - 1
-
-print("値がnullでない最後の成分は"+comp[cnt])
-
-print("-------------------------------------------------------")
-
-for i in range(cnt):
-    if value[i] != "null":
-        if unit[i] != "null":
-            sys.stdout.write(comp[i]+" "+value[i]+" "+unit[i]+", ")
-        else:
-            sys.stdout.write(comp[i]+" "+value[i]+", ")
-
-if value[cnt] != "null":
-    if unit[cnt] != "null":
-        print(comp[cnt]+" "+value[cnt]+" "+unit[cnt])
-    else:
-        print(comp[cnt]+" "+value[cnt])
-
-print("-------------------------------------------------------")
-
